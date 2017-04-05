dbe747b04546db18914625dbe35a20838560f343
Jeremy Turowetz <j.turowetz@gmail.com>
2017-04-05 10:36:07 -0400

    remove f_file_manager

--- zf_file_manager/admin/js/tzCheckbox/script.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function($) {
-	$("div.b5_file_manager_theme_column input[type='checkbox']").tzCheckbox({labels:[b5_file_manager_tzchekbox.b5_file_manager_enable,b5_file_manager_tzchekbox.b5_file_manager_disable]});
-});
