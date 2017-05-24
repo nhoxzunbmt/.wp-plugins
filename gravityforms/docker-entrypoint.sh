6bb97ce16c29da288cdded6daf53ca4cf49af9ca
Jeremy Turowetz <j.turowetz@gmail.com>
2017-05-24 11:04:41 -0400

    remove old plugins

--- gravityforms/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Remove the default wp-config.php file becaue it doesn't set SCRIPT_DEBUG
-rm /wp-core/wp-config.php
-
-# Add the custom wp-config-php file
-cp /project/tests/acceptance-tests/wp-config-codeception.php /wp-core/wp-config.php
-
-exec "/repo/vendor/bin/codecept" "$@"
